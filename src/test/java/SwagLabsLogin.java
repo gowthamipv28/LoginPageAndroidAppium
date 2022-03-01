4e01bf8161317f449ca1fe6c89cf574392b50705
gowthamipv <gowthamipv@testvagrant.com>
2022-03-01 13:40:44 +0530

    This is my First Appium project

---
+++ src/test/java/SwagLabsLogin.java
@@ -0,0 +1,32 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SwagLabsLogin{
+@Test
+    public void login() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel 3");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("appPackage", "com.swaglabsmobileapp");
+        capabilities.setCapability("appActivity", "com.swaglabsmobileapp.MainActivity");
+
+
+    WebDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Username\"]")).sendKeys("standard_user");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).sendKeys("secret_sauce");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")).click();
+    }
+}
+
